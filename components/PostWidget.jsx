4092dfe96da35a346d65765b77b62f72537d42ad
enochnewton <enochnewton2@gmail.com>
2023-08-24 19:41:17 +0300

    7th

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,29 +2,21 @@
 import { useSelector } from "react-redux";
 import Post from "./Post";
 import Skeleton from "@mui/material/Skeleton";
-import { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 
 const PostWidget = ({ myPosts = false }) => {
   const user = useSelector(state => state.user);
   const posts = useSelector(state => state.posts);
-  const [showSkeleton, setShowSkeleton] = useState(false);
 
-  useEffect(() => {
-    if (!posts || posts.length === 0) {
-      setShowSkeleton(true);
+  if (myPosts && posts.length === 0) {
+    return (
+      <Typography variant='h6' sx={{ textAlign: "center", mt: "20px" }}>
+        You have no posts yet
+      </Typography>
+    );
+  }
 
-      const timer = setTimeout(() => {
-        setShowSkeleton(false);
-      }, 5000); // 5000 milliseconds = 5 seconds
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [posts]);
-
-  if (showSkeleton) {
+  if (!posts || posts.length === 0) {
     return (
       <Skeleton
         variant='rectangular'
@@ -32,20 +24,6 @@ const PostWidget = ({ myPosts = false }) => {
         height='447px'
         animation='wave'
       />
-    );
-  } else if (posts.length === 0) {
-    return (
-      <Typography
-        variant='h5'
-        sx={{
-          color: "text.secondary",
-          textAlign: "center",
-          mt: "50px",
-          fontWeight: "bold",
-        }}
-      >
-        No Posts
-      </Typography>
     );
   }
 
